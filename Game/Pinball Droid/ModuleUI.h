54a08dabfcbc27c1aa3e58e81201967b67dc3703
tomascarreras1000 <tomascarreras1000@gmail.com>
2019-11-03 22:15:12 +0100

    Update ModuleUI.h

--- Game/Pinball Droid/ModuleUI.h
+++ Game/Pinball Droid/ModuleUI.h
@@ -2,34 +2,6 @@
 #include "Globals.h"
 #include "p2Point.h"
 
-class ModuleUI : public Module
-{
-public:
-	ModuleUI(Application* app, bool start_enabled = true);
-	virtual ~ModuleUI();
-
-	bool Start();
-	update_status Update();
-	bool CleanUp();
-
-public:
-	int cont = 0;
-	int font = 0;
-	int Score = 0;
-	int LastScore = 0;
-	int HighScore = 0;
-	int Aux[100];
-	char score[10];
-	char lastscore[10];
-	char highscore[10];
-};
-
-#if 0;
-
-#include "Module.h"
-#include "Globals.h"
-#include "p2Point.h"
-
 struct SDL_Texture;
 
 class ModuleUI : public Module
@@ -58,5 +30,3 @@ public:
 	int current_time = 0, time_on_entry = 0;
 
 };
-
-#endif;
